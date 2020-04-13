f47d72a6605f7e24081351f1fcd509265334f568
Maurice Cooper <jc70240@gmail.com>
2020-04-13 04:46:21 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super()
+
+    this.state = {
+      flags: [
+        {
+          name: 'Japan',
+          id: '1'
+        },
+        {
+          name: 'Kenya',
+          id: '2'
+        },
+        {
+          name: 'Korea',
+          id: '3'
+        },
+      ]
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        { this.state.flags.map(flag => 
+          <h1 key={flag.id}> {flag.name} </h1>
+        )}
+      </div>
+    )
+  }
 }
 
 export default App;
