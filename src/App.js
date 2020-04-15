a54957f9a7d8d2c5fe50d45ad6a537a6739f5f42
Maurice Cooper <jc70240@gmail.com>
2020-04-15 03:51:06 -0500

    working on toggling images on click

--- src/App.js
+++ src/App.js
@@ -2,48 +2,68 @@ import React, { Component } from 'react';
 import { CardList } from './components/card_list/card_list.component'
 import './App.css';
 
+
 class App extends Component{
   constructor(){
     super()
 
     this.state = {
+      open: true,
       flags: [
         {
           name: 'Japan',
-          id: '1'
+          id: '1',
+          images: '/japan-flag.png'
         },
         {
           name: 'Kenya',
-          id: '2'
+          id: '2',
+          images: 'kenya-flag.png'
         },
         {
           name: 'Korea',
-          id: '3'
+          id: '3',
+          images: 'korea-flag.png'
         },
         {
           name: 'Ghana',
-          id: '4'
+          id: '4',
+          images: 'ghana-flag.jpg'
         },
         {
           name: 'Qatar',
-          id: '5'
+          id: '5',
+          images: 'qatar-flag.png'
         },
         {
           name: 'Cameroon',
-          id: '6'
+          id: '6',
+          images: 'cameroon-flag.png'
         },
         {
           name: 'Nigeria',
-          id: '7'
-        },
+          id: '7',
+          images: 'nigeria-glag.png'
+        }
       ]
     }
+
+  }
+  
+
+  toggleImage = () => {
+    this.setState(state => ({
+      open: !state.open
+    }))
   }
 
   render(){
     return (
       <div className="App">
         <CardList flags={this.state.flags} />
+        <button onClick={this.toggleImage}>
+          {this.state.open ? 'Switch me back!' : 'View Flags!'}
+        </button>
       </div>
     )
   }
